b99ccd15b3f10d136b853802354731a29c5d8c58
tharuka-pavith <tharukapavith2@gmail.com>
2022-02-24 23:32:52 +0530

    update main.py

--- workspace/004-wifi-main/main.py
+++ workspace/004-wifi-main/main.py
@@ -3,17 +3,22 @@
 
 import network
 
+ACTIVATE_STA = False #Toggle here to activate/deactivate
+
 sta_if = network.WLAN(network.STA_IF)
 
-sta_if.active(True)
+sta_if.active(ACTIVATE_STA)
 
-try:
-  sta_if.connect('AP J6', 'mcu-8266')
-  print('connected to AP')
-except:
-  print("Error: cannot connnect to AP")
-finally:
-  gc.collect()
+if ACTIVATE_STA: 
+  try:
+    sta_if.connect('AP J6', 'mcu-8266')
+    if sta_if.isconnected(): 
+      print('INFO: connected to AP')
+  except:
+    print("Error: cannot connnect to AP")
+  finally:
+    gc.collect()
+
 
 
 
